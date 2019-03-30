eadc46466f033c5e6040c82b0b16de8b3d77e512
Umputun <umputun@gmail.com>
2019-03-30 15:09:23 -0500

    Formatter (#12)
    * WIP: add template formatter
    
    * partial compatibility with prev version
    
    * handle braces, add docs about templating
    
    * more explicit levels brace replacement
    
    * add TRACE level
    
    * handle valid template with invalid var in

--- adaptor_test.go
+++ adaptor_test.go
@@ -11,14 +11,14 @@ import (
 
 func TestAdaptor_ToWriter(t *testing.T) {
 	rout, rerr := bytes.NewBuffer([]byte{}), bytes.NewBuffer([]byte{})
-	l := New(Out(rout), Err(rerr), Msec, LevelBraces)
+	l := New(Out(rout), Err(rerr), Format(WithMsec))
 	l.now = func() time.Time { return time.Date(2018, 1, 7, 13, 2, 34, 0, time.Local) }
 
 	wr := ToWriter(l, "WARN")
 	sz, err := wr.Write([]byte("something blah 123"))
 	require.NoError(t, err)
 	assert.Equal(t, 18, sz)
-	assert.Equal(t, "2018/01/07 13:02:34.000 [WARN]  something blah 123\n", rout.String())
+	assert.Equal(t, "2018/01/07 13:02:34.000 WARN  something blah 123\n", rout.String())
 }
 
 func TestAdaptor_ToWriterNoLevel(t *testing.T) {
@@ -41,15 +41,15 @@ func TestAdaptor_ToWriterNoLevel(t *testing.T) {
 
 func TestAdaptor_ToStdLogger(t *testing.T) {
 	rout, rerr := bytes.NewBuffer([]byte{}), bytes.NewBuffer([]byte{})
-	l := New(Out(rout), Err(rerr), Msec, LevelBraces)
+	l := New(Out(rout), Err(rerr), Format(WithMsec))
 	l.now = func() time.Time { return time.Date(2018, 1, 7, 13, 2, 34, 0, time.Local) }
 
 	wr := ToStdLogger(l, "WARN")
 	wr.Print("something")
-	assert.Equal(t, "2018/01/07 13:02:34.000 [WARN]  something\n", rout.String())
+	assert.Equal(t, "2018/01/07 13:02:34.000 WARN  something\n", rout.String())
 
 	rout.Reset()
 	rerr.Reset()
 	wr.Printf("xxx %s", "yyy")
-	assert.Equal(t, "2018/01/07 13:02:34.000 [WARN]  xxx yyy\n", rout.String())
+	assert.Equal(t, "2018/01/07 13:02:34.000 WARN  xxx yyy\n", rout.String())
 }
--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package lgr
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -8,36 +9,62 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+	"text/template"
 	"time"
 )
 
-var levels = []string{"DEBUG", "INFO", "WARN", "ERROR", "PANIC", "FATAL"}
+var levels = []string{"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "PANIC", "FATAL"}
+
+const (
+	Short      = `{{.DT.Format "2006/01/02 15:04:05"}} {{.Level}} {{.Message}}`
+	WithMsec   = `{{.DT.Format "2006/01/02 15:04:05.000"}} {{.Level}} {{.Message}}`
+	WithPkg    = `{{.DT.Format "2006/01/02 15:04:05.000"}} {{.Level}} ({{.CallerPkg}}) {{.Message}}`
+	ShortDebug = `{{.DT.Format "2006/01/02 15:04:05.000"}} {{.Level}} ({{.CallerFile}}:{{.CallerLine}}) {{.Message}}`
+	FuncDebug  = `{{.DT.Format "2006/01/02 15:04:05.000"}} {{.Level}} ({{.CallerFunc}}) {{.Message}}`
+	FullDebug  = `{{.DT.Format "2006/01/02 15:04:05.000"}} {{.Level}} ({{.CallerFile}}:{{.CallerLine}} {{.CallerFunc}}) {{.Message}}`
+)
 
 // Logger provided simple logger with basic support of levels. Thread safe
 type Logger struct {
 	// set with Option calls
 	stdout, stderr io.Writer // destination writes for out and err
 	dbg            bool      // allows reporting for DEBUG level
+	trace          bool      // allows reporting for TRACE and DEBUG levels
 	callerFile     bool      // reports caller file, i.e. /go/src/github.com/go-pkgz/lgr/logger.go
 	callerFunc     bool      // reports caller function name, i.e. foo/bar.myFunc
 	callerPkg      bool      // reports caller package name
 	levelBraces    bool      // encloses level with [], i.e. [INFO]
 	callerDepth    int       // how many stack frames to skip
+	format         string    // layout template
 
 	// internal use
-	now   nowFn
-	fatal panicFn
-	msec  bool
-	lock  sync.Mutex
+	now           nowFn
+	fatal         panicFn
+	msec          bool
+	lock          sync.Mutex
+	callerOn      bool
+	levelBracesOn bool
+	templ         *template.Template
 }
 
 // can be redefined internally for testing
 type nowFn func() time.Time
 type panicFn func()
 
+type layout struct {
+	DT         time.Time
+	Level      string
+	Message    string
+	CallerPkg  string
+	CallerFile string
+	CallerFunc string
+	CallerLine int
+}
+
 // New makes new leveled logger. Accepts dbg flag turing on info about the caller and allowing DEBUG messages.
 // Two writers can be passed optionally - first for out and second for err
 func New(options ...Option) *Logger {
+
 	res := Logger{
 		now:         time.Now,
 		fatal:       func() { os.Exit(1) },
@@ -48,6 +75,28 @@ func New(options ...Option) *Logger {
 	for _, opt := range options {
 		opt(&res)
 	}
+
+	var err error
+	if res.format == "" {
+		res.format = res.templateFromOptions()
+	}
+
+	res.templ, err = template.New("lgr").Parse(res.format)
+	if err != nil {
+		fmt.Printf("invalid template %s, error %v. switched to %s\n", res.format, err, Short)
+		res.format = Short
+		res.templ = template.Must(template.New("lgrDefault").Parse(Short))
+	}
+
+	buf := bytes.Buffer{}
+	if err = res.templ.Execute(&buf, layout{}); err != nil {
+		fmt.Printf("failed to execute template %s, error %v. switched to %s\n", res.format, err, Short)
+		res.format = Short
+		res.templ = template.Must(template.New("lgrDefault").Parse(Short))
+	}
+
+	res.callerOn = strings.Contains(res.format, "{{.Caller")
+	res.levelBracesOn = strings.Contains(res.format, "[{{.Level}}]")
 	return &res
 }
 
@@ -62,57 +111,67 @@ func (l *Logger) Logf(format string, args ...interface{}) {
 
 func (l *Logger) logf(format string, args ...interface{}) {
 
-	// format timestamp with or without msecs
-	ts := func() (res string) {
-		if l.msec {
-			return l.now().Format("2006/01/02 15:04:05.000")
-		}
-		return l.now().Format("2006/01/02 15:04:05")
-	}
-
 	lv, msg := l.extractLevel(fmt.Sprintf(format, args...))
 	if lv == "DEBUG" && !l.dbg {
 		return
 	}
-	var bld strings.Builder
-	bld.WriteString(ts())
-	bld.WriteString(l.formatLevel(lv))
-	bld.WriteString(" ")
-
-	if caller := l.reportCaller(l.callerDepth); caller != "" {
-		bld.WriteString("{")
-		bld.WriteString(caller)
-		bld.WriteString("} ")
+	if lv == "TRACE" && !l.trace {
+		return
 	}
 
-	bld.WriteString(msg)
+	ci := callerInfo{}
+	if l.callerOn { // optimization to avlod expensive caller evaluation if not in template
+		ci = l.reportCaller(l.callerDepth)
+	}
 
-	if !strings.HasSuffix(msg, "\n") { // A newline is appended if the last character of s is not already a newline.
-		bld.WriteString("\n")
+	elems := layout{
+		DT:         l.now(),
+		Level:      l.formatLevel(lv),
+		Message:    strings.TrimSuffix(msg, "\n"),
+		CallerFunc: ci.FuncName,
+		CallerFile: ci.File,
+		CallerPkg:  ci.Pkg,
+		CallerLine: ci.Line,
+	}
+
+	buf := bytes.Buffer{}
+	err := l.templ.Execute(&buf, elems) // once constructed, a template may be executed safely in parallel.
+	if err != nil {
+		fmt.Printf("failed to execute template, %v\n", err)
+	}
+	buf.WriteString("\n")
+
+	data := buf.Bytes()
+	if l.levelBracesOn {
+		data = bytes.Replace(data, []byte("[WARN ]"), []byte("[WARN] "), 1)
+		data = bytes.Replace(data, []byte("[INFO ]"), []byte("[INFO] "), 1)
 	}
 
 	l.lock.Lock()
-	msgb := []byte(bld.String())
-	_, _ = l.stdout.Write(msgb)
+	_, _ = l.stdout.Write(data)
 
+	// write to err as well for high levels
 	switch lv {
 	case "PANIC", "FATAL":
-		_, _ = l.stderr.Write(msgb)
+		_, _ = l.stderr.Write(data)
 		_, _ = l.stderr.Write(getDump())
 		l.fatal()
 	case "ERROR":
-		_, _ = l.stderr.Write(msgb) //nolint
+		_, _ = l.stderr.Write(data)
 	}
 
 	l.lock.Unlock()
 }
 
-// calldepth 0 identifying the caller of reportCaller()
-func (l *Logger) reportCaller(calldepth int) string {
+type callerInfo struct {
+	File     string
+	Line     int
+	FuncName string
+	Pkg      string
+}
 
-	if !(l.callerFile || l.callerFunc || l.callerPkg) {
-		return ""
-	}
+// calldepth 0 identifying the caller of reportCaller()
+func (l *Logger) reportCaller(calldepth int) (res callerInfo) {
 
 	// caller gets file, line number abd function name via runtime.Callers
 	// file looks like /go/src/github.com/go-pkgz/lgr/logger.go
@@ -137,48 +196,62 @@ func (l *Logger) reportCaller(calldepth int) string {
 		return frame.File, frame.Line, frame.Function
 	}
 
-	// add 5 to adjust stack level because it was called from 3 nested functions added by lgr, i.e. caller, reportCaller and logf, plus 2 frames by runtime
+	// add 5 to adjust stack level because it was called from 3 nested functions added by lgr, i.e. caller,
+	// reportCaller and logf, plus 2 frames by runtime
 	filePath, line, funcName := caller(calldepth + 2 + 3)
 	if (filePath == "") || (line <= 0) || (funcName == "") {
-		return "???"
+		return callerInfo{}
 	}
 
-	// callerPkg only if no other callers
-	if l.callerPkg && !l.callerFile && !l.callerFunc {
-		_, pkgInfo := path.Split(path.Dir(filePath))
-		return pkgInfo
-	}
+	_, pkgInfo := path.Split(path.Dir(filePath))
+	res.Pkg = pkgInfo
 
-	res := ""
-
-	if l.callerFile {
-		fileInfo := filePath
-		if pathElems := strings.Split(filePath, "/"); len(pathElems) > 2 {
-			fileInfo = strings.Join(pathElems[len(pathElems)-2:], "/")
-		}
-		res += fmt.Sprintf("%s:%d", fileInfo, line)
-		if l.callerFunc {
-			res += " "
-		}
+	res.File = filePath
+	if pathElems := strings.Split(filePath, "/"); len(pathElems) > 2 {
+		res.File = strings.Join(pathElems[len(pathElems)-2:], "/")
 	}
+	res.Line = line
 
-	if l.callerFunc {
-		funcNameElems := strings.Split(funcName, "/")
-		funcInfo := funcNameElems[len(funcNameElems)-1]
-		res += funcInfo
-	}
+	funcNameElems := strings.Split(funcName, "/")
+	res.FuncName = funcNameElems[len(funcNameElems)-1]
 
 	return res
 }
 
-func (l *Logger) formatLevel(lv string) string {
+// make template from options flag
+func (l *Logger) templateFromOptions() (res string) {
 
-	brace := func(b string) string {
-		if l.levelBraces {
-			return b
+	orElse := func(flag bool, value string, elseValue string) string {
+		if flag {
+			return value
 		}
-		return ""
+		return elseValue
 	}
+
+	var parts []string
+
+	parts = append(parts, orElse(l.msec, `{{.DT.Format "2006/01/02 15:04:05.000"}}`, `{{.DT.Format "2006/01/02 15:04:05"}}`))
+	parts = append(parts, orElse(l.levelBraces, `[{{.Level}}]`, `{{.Level}}`))
+
+	if l.callerFile || l.callerFunc || l.callerPkg {
+		var callerParts []string
+		if v := orElse(l.callerFile, `{{.CallerFile}}:{{.CallerLine}}`, ""); v != "" {
+			callerParts = append(callerParts, v)
+		}
+		if v := orElse(l.callerFunc, `{{.CallerFunc}}`, ""); v != "" {
+			callerParts = append(callerParts, v)
+		}
+		if v := orElse(l.callerPkg, `{{.CallerPkg}}`, ""); v != "" {
+			callerParts = append(callerParts, v)
+		}
+		parts = append(parts, "("+strings.Join(callerParts, " ")+")")
+	}
+	parts = append(parts, "{{.Message}}")
+	return strings.Join(parts, " ")
+}
+
+// formatLevel aligns level to 5 chars
+func (l *Logger) formatLevel(lv string) string {
 
 	if lv == "" {
 		return ""
@@ -188,9 +261,10 @@ func (l *Logger) formatLevel(lv string) string {
 	if len(lv) == 4 {
 		spaces = " "
 	}
-	return " " + brace("[") + lv + brace("]") + spaces
+	return lv + spaces
 }
 
+// extractLevel parses messages with optional level prefix and returns level and the message with stripped level
 func (l *Logger) extractLevel(line string) (level, msg string) {
 	for _, lv := range levels {
 		if strings.HasPrefix(line, lv) {
@@ -236,10 +310,22 @@ func Debug(l *Logger) {
 	l.dbg = true
 }
 
+// Trace turn on trace + dbg mode
+func Trace(l *Logger) {
+	l.dbg = true
+	l.trace = true
+}
+
 // CallerDepth sets number of stack frame skipped for caller reporting
 func CallerDepth(n int) Option {
 	return func(l *Logger) {
 		l.callerDepth = n
+	}
+}
+
+func Format(f string) Option {
+	return func(l *Logger) {
+		l.format = f
 	}
 }
 
